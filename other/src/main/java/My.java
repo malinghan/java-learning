39123db06d5f8746727685e4361287e15e73fd4b
马凌汉 <malinghan@qiyeyoudao.com>
2018-06-21 23:17:15 +0800

    sa

--- other/src/main/java/My.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author: linghan.ma
- * @DATE: 2018/1/12
- * @description:
- */
-public class My {
-}
--- other/src/test/java/Course.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author: linghan.ma
- * @DATE: 2018/4/12
- * @description:
- */
-public class Course {
-}
