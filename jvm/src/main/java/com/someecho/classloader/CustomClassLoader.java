849e3387b4b348dc7a15dc8ce448c69083745864
malinghan <linghan.ma@gmail.com>
2024-02-28 00:12:13 +0800

    push

---
+++ jvm/src/main/java/com/someecho/classloader/CustomClassLoader.java
@@ -0,0 +1,5 @@
+package com.someecho.classloader;/**
+ * @author someecho <linghan.ma@gmail.com>
+ * Created on 2024-02-27
+ */public class CustomClassLoader {
+}
--- jvm/src/main/java/com/someecho/gc/GCLogAnalysis.java
+++ jvm/src/main/java/com/someecho/gc/GCLogAnalysis.java
@@ -1,4 +1,4 @@
-package com.someecho;
+package com.someecho.gc;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
